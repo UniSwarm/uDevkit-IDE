b3e423b3a16057211faa38b979412247bc2418da
sebcaux <sebastien.caux@robotips.fr>
2017-12-06 09:00:05 +0100

    [icondock] Fixed icon pos, removed windows style EOF

--- src/ui/iconneddockstyle.cpp
+++ src/ui/iconneddockstyle.cpp
@@ -3,23 +3,22 @@
 IconnedDockStyle::IconnedDockStyle(const QIcon &icon, QStyle *style)
     : QProxyStyle(style), _icon(icon)
 {
-
 }
 
 void IconnedDockStyle::drawControl(QStyle::ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
 {
-    if(element == QStyle::CE_DockWidgetTitle)
+    if (element == QStyle::CE_DockWidgetTitle)
     {
         //width of the icon
         int width = pixelMetric(QStyle::PM_ToolBarIconSize);
         //margin of title from frame
         int margin = baseStyle()->pixelMetric(QStyle::PM_DockWidgetTitleMargin);
 
-        QPoint iconPos(margin + option->rect.left(), margin + option->rect.center().y() - width * 2 / 3);
+        QPoint iconPos(margin + option->rect.left() + width / 6, margin + option->rect.y() + width / 6);
 
-        painter->drawPixmap(iconPos, _icon.pixmap(width, width));
+        painter->drawPixmap(iconPos, _icon.pixmap(width * 2 / 3, width * 2 / 3));
 
-        const_cast<QStyleOption*>(option)->rect = option->rect.adjusted(width * 3 / 2, 0, 0, 0);
+        const_cast<QStyleOption*>(option)->rect = option->rect.adjusted(width + margin * 2, 0, 0, 0);
     }
     baseStyle()->drawControl(element, option, painter, widget);
 }
